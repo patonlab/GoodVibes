6cbda89054d5da5fd8294ec240009becf9a3a8c5
Robert Paton <robertpaton@gmail.com>
2017-04-12 21:04:56 +0100

    Added config files for pypi

---
+++ setup.py
@@ -0,0 +1,13 @@
+from distutils.core import setup
+setup(
+  name = 'goodvibes',
+  packages = ['goodvibes'], 
+  version = '2.0.1',
+  description = 'Calculates quasi-harmonic free energies from Gaussian output files with temperature and haptic corrections',
+  author = 'Robert Paton',
+  author_email = 'robert.paton@chem.ox.ac.uk',
+  url = 'https://github.com/bobbypaton/goodvibes', 
+  download_url = 'https://zenodo.org/record/60811/files/GoodVibes-1.0.1.zip', 
+  keywords = ['compchem', 'thermochemistry', 'gaussian', 'vibrational-entropies', 'temperature'], 
+  classifiers = [],
+)
