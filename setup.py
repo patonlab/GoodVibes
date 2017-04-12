813be21b4d9a9adbd9e5ba466196b54962274f0d
Robert Paton <robertpaton@gmail.com>
2017-04-12 21:06:33 +0100

    updated version no in setup.py

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from distutils.core import setup
 setup(
   name = 'goodvibes',
   packages = ['goodvibes'], 
-  version = '2.0.1',
+  version = '1.0.1',
   description = 'Calculates quasi-harmonic free energies from Gaussian output files with temperature and haptic corrections',
   author = 'Robert Paton',
   author_email = 'robert.paton@chem.ox.ac.uk',
