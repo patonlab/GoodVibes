47cdabc9dc67fe38ef2c7edec36adc112810a137
Robert Paton <robert.paton@Roberts-MacBook-Pro.local>
2017-09-04 22:44:15 +0200

    Writes Cartesians to xyz file

--- goodvibes/GoodVibes.py
+++ goodvibes/GoodVibes.py
@@ -24,10 +24,10 @@ from __future__ import print_function
 #  according to the amount of free space available.                   #
 #######################################################################
 #######  Written by:  Rob Paton and Ignacio Funes-Ardoiz ##############
-#######  Last modified:  Aug 10, 2017 #################################
+#######  Last modified:   Sep 4, 2017 #################################
 #######################################################################
 
-import sys, math, textwrap, time
+import os.path, sys, math, textwrap, time
 from glob import glob
 from optparse import OptionParser
 
@@ -47,38 +47,93 @@ grimme_ref = "Grimme, S. Chem. Eur. J. 2012, 18, 9955-9964"
 truhlar_ref = "Ribeiro, R. F.; Marenich, A. V.; Cramer, C. J.; Truhlar, D. G. J. Phys. Chem. B 2011, 115, 14556-14562"
 goodvibes_ref = "Funes-Ardoiz, I.; Paton, R. S. (2016). GoodVibes: GoodVibes v1.0.2. http://doi.org/10.5281/zenodo.595246"
 
-# Enables output to terminal and to text file
+#Some useful arrays
+periodictable = ["","H","He","Li","Be","B","C","N","O","F","Ne","Na","Mg","Al","Si","P","S","Cl","Ar","K","Ca","Sc","Ti","V","Cr","Mn","Fe","Co","Ni","Cu","Zn","Ga","Ge","As","Se","Br","Kr","Rb","Sr","Y","Zr",
+    "Nb","Mo","Tc","Ru","Rh","Pd","Ag","Cd","In","Sn","Sb","Te","I","Xe","Cs","Ba","La","Ce","Pr","Nd","Pm","Sm","Eu","Gd","Tb","Dy","Ho","Er","Tm","Yb","Lu","Hf","Ta","W","Re","Os","Ir","Pt","Au","Hg","Tl",
+    "Pb","Bi","Po","At","Rn","Fr","Ra","Ac","Th","Pa","U","Np","Pu","Am","Cm","Bk","Cf","Es","Fm","Md","No","Lr","Rf","Db","Sg","Bh","Hs","Mt","Ds","Rg","Uub","Uut","Uuq","Uup","Uuh","Uus","Uuo"]
+
+def elementID(massno):
+    if massno < len(periodictable): return periodictable[massno]
+    else: return "XX"
+
+ # Enables output to terminal and to text file
 class Logger:
-   # Designated initializer
-   def __init__(self,filein,suffix,append):
+   def __init__(self, filein, suffix, append):
       self.log = open(filein+"_"+append+"."+suffix, 'w' )
 
    def Write(self, message):
-      # Print the message and write to log
       print(message, end='')
       self.log.write(message)
 
    def Fatal(self, message):
-      # Print the message, write to log, finalize log file and stop program
       print(message+"\n")
-      self.log.write(message + "\n")
-      self.Finalize()
+      self.log.write(message + "\n"); self.Finalize()
       sys.exit(1)
 
-   # Finalize the log file
    def Finalize(self):
       self.log.close()
 
+# Enables output of optimized coordinates to a single xyz-formatted file
+class XYZout:
+   def __init__(self, filein, suffix, append):
+      self.xyz = open(filein+"_"+append+"."+suffix, 'w' )
+
+   def Writetext(self, message):
+      self.xyz.write(message + "\n")
+
+   def Writecoords(self, atoms, coords):
+      for n, carts in enumerate(coords):
+          self.xyz.write('{:>1}'.format(atoms[n]))
+          for cart in carts: self.xyz.write('{:13.6f}'.format(cart))
+          self.xyz.write('\n')
+
+   def Finalize(self):
+      self.xyz.close()
+
+#Read molecule data from a compchem output file
+class getoutData:
+    def __init__(self, file):
+        with open(file) as f: data = f.readlines()
+        program = 'none'
+
+        for line in data:
+           if line.find("Gaussian") > -1: program = "Gaussian"; break
+
+        def getATOMTYPES(self, outlines, program):
+            if program == "Gaussian":
+                for i, line in enumerate(outlines):
+                    if line.find("Input orientation") >-1 or line.find("Standard orientation") > -1:
+                        self.ATOMTYPES, self.CARTESIANS, self.ATOMICTYPES, carts = [], [], [], outlines[i+5:]
+                        for j, line in enumerate(carts):
+                            if line.find("-------") > -1: break
+                            self.ATOMTYPES.append(elementID(int(line.split()[1])))
+                            self.ATOMICTYPES.append(int(line.split()[2]))
+                            if len(line.split()) > 5: self.CARTESIANS.append([float(line.split()[3]),float(line.split()[4]),float(line.split()[5])])
+                            else: self.CARTESIANS.append([float(line.split()[2]),float(line.split()[3]),float(line.split()[4])])
+
+        getATOMTYPES(self, data, program)
+
 # Read gaussian output for a single point energy
 def sp_energy(file):
-   with open(file) as f: data = f.readlines()
-   spe = 'none'
+   spe, program, data = 'none', 'none', []
+
+   if os.path.exists(file.split('.')[0]+'.log'):
+       with open(file.split('.')[0]+'.log') as f: data = f.readlines()
+   if os.path.exists(file.split('.')[0]+'.out'):
+       with open(file.split('.')[0]+'.out') as f: data = f.readlines()
+
    for line in data:
-       if line.strip().startswith('SCF Done:'):
-           spe = float(line.strip().split()[4])
+       if line.find("Gaussian") > -1: program = "Gaussian"; break
+       if line.find("* O   R   C   A *") > -1: program = "Orca"; break
+
+   for line in data:
+       if program == "Gaussian":
+           if line.strip().startswith('SCF Done:'): spe = float(line.strip().split()[4])
+       if program == "Orca":
+           if line.strip().startswith('FINAL SINGLE POINT ENERGY'): spe = float(line.strip().split()[4])
    return spe
 
-# Read gaussian output for the level of theory and basis set used
+# Read output for the level of theory and basis set used
 def level_of_theory(file):
    with open(file) as f: data = f.readlines()
    level, bs = 'none', 'none'
@@ -94,9 +149,8 @@ def level_of_theory(file):
 # translational energy evaluation (depends on temperature)
 def calc_translational_energy(temperature):
    """
-   Calculates the translational energy (J/mol) of an ideal gas - i.e.
-   non-interactiing molecules so molar energy = Na * atomic energy
-   This approximxation applies to all energies and entropies computed within
+   Calculates the translational energy (J/mol) of an ideal gas - i.e. non-interactiing molecules so molar energy = Na * atomic energy
+   This approximation applies to all energies and entropies computed within
    Etrans = 3/2 RT!
    """
    energy = 1.5 * GAS_CONSTANT * temperature
@@ -116,8 +170,7 @@ def calc_rotational_energy(zpe, symmno, temperature, linear):
 # vibrational energy evaluation (depends on frequencies, temperature and scaling factor: default = 1.0)
 def calc_vibrational_energy(frequency_wn, temperature, freq_scale_factor):
    """
-   Calculates the vibrational energy contribution (J/mol)
-   Includes ZPE (0K) and thermal contributions
+   Calculates the vibrational energy contribution (J/mol). Includes ZPE (0K) and thermal contributions
    Evib = R * Sum(0.5 hv/k + (hv/k)/(e^(hv/KT)-1))
    """
    factor = [(PLANCK_CONSTANT * freq * SPEED_OF_LIGHT * freq_scale_factor) / (BOLTZMANN_CONSTANT * temperature) for freq in frequency_wn]
@@ -130,7 +183,6 @@ def calc_zeropoint_energy(frequency_wn, freq_scale_factor):
    Calculates the vibrational ZPE (J/mol)
    EZPE = Sum(0.5 hv/k)
    """
-   frequency = [entry * SPEED_OF_LIGHT for entry in frequency_wn]
    factor = [PLANCK_CONSTANT * freq * SPEED_OF_LIGHT * freq_scale_factor / BOLTZMANN_CONSTANT for freq in frequency_wn]
    energy = [0.5 * entry * GAS_CONSTANT for entry in factor]
    return sum(energy)
@@ -138,8 +190,7 @@ def calc_zeropoint_energy(frequency_wn, freq_scale_factor):
 # Computed the amount of accessible free space (ml per L) in solution accesible to a solute immersed in bulk solvent, i.e. this is the volume not occupied by solvent molecules, calculated using literature values for molarity and B3LYP/6-31G* computed molecular volumes.
 def get_free_space(solv):
    """
-   Calculates the free space in a litre of bulk solvent,
-   based on Shakhnovich and Whitesides (J. Org. Chem. 1998, 63, 3821-3830)
+   Calculates the free space in a litre of bulk solvent, based on Shakhnovich and Whitesides (J. Org. Chem. 1998, 63, 3821-3830)
    """
    solvent_list = ["none", "H2O", "toluene", "DMF", "AcOH", "chloroform"]
    molarity = [1.0, 55.6, 9.4, 12.9, 17.4, 12.5] #mol/l
@@ -161,8 +212,7 @@ def get_free_space(solv):
 # translational entropy evaluation (depends on mass, concentration, temperature, solvent free space: default = 1000.0)
 def calc_translational_entropy(molecular_mass, conc, temperature, solv):
    """
-   Calculates the translational entropic contribution (J/(mol*K)) of an ideal gas
-   needs the molecular mass. Convert mass in amu to kg; conc in mol/l to number per m^3
+   Calculates the translational entropic contribution (J/(mol*K)) of an ideal gas. Needs the molecular mass. Convert mass in amu to kg; conc in mol/l to number per m^3
    Strans = R(Ln(2pimkT/h^2)^3/2(1/C)) + 1 + 3/2)
    """
    lmda = ((2.0 * math.pi * molecular_mass * AMU_to_KG * BOLTZMANN_CONSTANT * temperature)**0.5) / PLANCK_CONSTANT
@@ -218,7 +268,7 @@ def calc_freerot_entropy(frequency_wn, temperature, freq_scale_factor):
    Entropic contributions (J/(mol*K)) according to a free-rotor description for a list of vibrational modes
    Sr = R(1/2 + 1/2ln((8pi^3u'kT/h^2))
    """
-   #This is the average moment of inertia used by Grimme
+   # This is the average moment of inertia used by Grimme
    Bav = 10.0e-44
    mu = [PLANCK_CONSTANT / (8 * math.pi**2 * freq * SPEED_OF_LIGHT * freq_scale_factor) for freq in frequency_wn]
    mu_primed = [entry * Bav /(entry + Bav) for entry in mu]
@@ -228,9 +278,6 @@ def calc_freerot_entropy(frequency_wn, temperature, freq_scale_factor):
 
 # A damping function to interpolate between RRHO and free rotor vibrational entropy values
 def calc_damp(frequency_wn, FREQ_CUTOFF):
-   """
-   Calculates the Head-Gordon damping function with alpha=4
-   """
    alpha = 4
    damp = [1 / (1+(FREQ_CUTOFF/entry)**alpha) for entry in frequency_wn]
    return damp
@@ -330,11 +377,12 @@ class calc_bbe:
          # Add terms (converted to au) to get Free energy - perform separately for harmonic and quasi-harmonic values out of interest
          self.enthalpy = self.scf_energy + (Utrans + Urot + Uvib + GAS_CONSTANT * temperature) / j_to_au
          # single point correction replaces energy from optimization with single point value
-         if hasattr(self, 'sp_energy'): self.enthalpy = self.enthalpy - self.scf_energy + self.sp_energy
+         if hasattr(self, 'sp_energy'):
+            try: self.enthalpy = self.enthalpy - self.scf_energy + self.sp_energy
+            except TypeError: pass
          self.zpe = ZPE / j_to_au
          self.entropy, self.qh_entropy = (Strans + Srot + h_Svib + Selec) / j_to_au, (Strans + Srot + qh_Svib + Selec) / j_to_au
          self.gibbs_free_energy, self.qh_gibbs_free_energy = self.enthalpy - temperature * self.entropy, self.enthalpy - temperature * self.qh_entropy
-
 
 if __name__ == "__main__":
    # Start a log for the results
@@ -342,35 +390,32 @@ if __name__ == "__main__":
 
    # get command line inputs. Use -h to list all possible arguments and default values
    parser = OptionParser(usage="Usage: %prog [options] <input1>.log <input2>.log ...")
-   parser.add_option("-t", dest="temperature", action="store",
-                  help="temperature (K) (default 298.15)", default="298.15", type="float", metavar="TEMP")
-   parser.add_option("-q", dest="QH", action="store",
-                  help="Type of quasi-harmonic correction (Grimme or Truhlar) (default Grimme)", default="grimme", type="string", metavar="QH")
-   parser.add_option("-f", dest="freq_cutoff", action="store",
-                  help="Cut-off frequency (wavenumbers) (default = 100)", default="100.0", type="float", metavar="FREQ_CUTOFF")
-   parser.add_option("-c", dest="conc", action="store",
-                  help="concentration (mol/l) (default 1 atm)", default="0.040876", type="float", metavar="CONC")
-   parser.add_option("-v", dest="freq_scale_factor", action="store",
-                  help="Frequency scaling factor (default 1)", default=False, type="float", metavar="SCALE_FACTOR")
-   parser.add_option("-s", dest="solv", action="store",
-                  help="Solvent (H2O, toluene, DMF, AcOH, chloroform) (default none)", default="none", type="string", metavar="SOLV")
-   parser.add_option("--spc", dest="spc", action="store",
-                help="Indicates single point corrections (default False)", type="string", default=False, metavar="SPC")
-   parser.add_option("--ti", dest="temperature_interval", action="store",
-                help="initial temp, final temp, step size (K)", default=False, metavar="TI")
-   parser.add_option("--ci", dest="conc_interval", action="store",
-                help="initial conc, final conc, step size (mol/l)", default=False, metavar="CI")
-
+   parser.add_option("-t", dest="temperature", action="store", help="temperature (K) (default 298.15)", default="298.15", type="float", metavar="TEMP")
+   parser.add_option("-q", dest="QH", action="store", help="Type of quasi-harmonic correction (Grimme or Truhlar) (default Grimme)", default="grimme", type="string", metavar="QH")
+   parser.add_option("-f", dest="freq_cutoff", action="store", help="Cut-off frequency (wavenumbers) (default = 100)", default="100.0", type="float", metavar="FREQ_CUTOFF")
+   parser.add_option("-c", dest="conc", action="store", help="concentration (mol/l) (default 1 atm)", default="0.040876", type="float", metavar="CONC")
+   parser.add_option("-v", dest="freq_scale_factor", action="store", help="Frequency scaling factor (default 1)", default=False, type="float", metavar="SCALE_FACTOR")
+   parser.add_option("-s", dest="solv", action="store", help="Solvent (H2O, toluene, DMF, AcOH, chloroform) (default none)", default="none", type="string", metavar="SOLV")
+   parser.add_option("--spc", dest="spc", action="store", help="Indicates single point corrections (default False)", type="string", default=False, metavar="SPC")
+   parser.add_option("--ti", dest="temperature_interval", action="store", help="initial temp, final temp, step size (K)", default=False, metavar="TI")
+   parser.add_option("--ci", dest="conc_interval", action="store", help="initial conc, final conc, step size (mol/l)", default=False, metavar="CI")
+   parser.add_option("--xyz", dest="xyz", action="store_true", help="write Cartesians to an xyz file (default False)", default=False, metavar="XYZ")
    (options, args) = parser.parse_args()
    options.QH = options.QH.lower() # case insensitive
 
-  # Get the filenames from the command line prompt
+   # if necessary create an xyz file for Cartesians
+   if options.xyz == True: xyz = XYZout("Goodvibes","xyz", "output")
+
+   # Get the filenames from the command line prompt
    files = []
    if len(sys.argv) > 1:
       for elem in sys.argv[1:]:
          try:
             if elem.split(".")[1] == "out" or elem.split(".")[1] == "log":
-               for file in glob(elem): files.append(file)
+               for file in glob(elem):
+                   if options.spc == False or options.spc == 'link': files.append(file)
+                   else:
+                       if file.find('_'+options.spc+".") == -1: files.append(file)
          except IndexError: pass
 
       # Start printing results
@@ -380,8 +425,7 @@ if __name__ == "__main__":
       if options.temperature_interval == False: log.Write("   Temperature = "+str(options.temperature)+" Kelvin")
       # If not at standard temp, need to correct the molarity of 1 atmosphere (assuming Pressure is still 1 atm)
       if options.conc == 0.040876:
-          options.conc = atmos/(GAS_CONSTANT*options.temperature)
-          log.Write("   Pressure = 1 atm")
+          options.conc = atmos/(GAS_CONSTANT*options.temperature); log.Write("   Pressure = 1 atm")
       else: log.Write("   Concentration = "+str(options.conc)+" mol/l")
 
       # attempt to automatically obtain frequency scale factor. Requires all outputs to be same level of theory
@@ -415,20 +459,25 @@ if __name__ == "__main__":
 
    # Standard mode: tabulate thermochemistry ouput from file(s) at a single temperature and concentration
    if options.temperature_interval == False and options.conc_interval == False:
-      if options.spc == False:
-          log.Write("\n\n   " + '{:<39} {:>13} {:>10} {:>13} {:>10} {:>10} {:>13} {:>13}'.format("Structure", "E", "ZPE", "H", "T.S", "T.qh-S", "G(T)", "qh-G(T)"))
-          log.Write("\n"+stars)
-      else:
-          log.Write("\n\n   " + '{:<39} {:>13} {:>13} {:>10} {:>13} {:>10} {:>10} {:>13} {:>13}'.format("Structure", "E_"+options.spc, "E", "ZPE", "H_"+options.spc, "T.S", "T.qh-S", "G(T)_"+options.spc, "qh-G(T)_"+options.spc))
-          log.Write("\n"+stars+'*'*14)
+      if options.spc == False: log.Write("\n\n   " + '{:<39} {:>13} {:>10} {:>13} {:>10} {:>10} {:>13} {:>13}'.format("Structure", "E", "ZPE", "H", "T.S", "T.qh-S", "G(T)", "qh-G(T)"))
+      else: log.Write("\n\n   " + '{:<39} {:>13} {:>13} {:>10} {:>13} {:>10} {:>10} {:>13} {:>13}'.format("Structure", "E_"+options.spc, "E", "ZPE", "H_"+options.spc, "T.S", "T.qh-S", "G(T)_"+options.spc, "qh-G(T)_"+options.spc))
+      if options.spc == False: log.Write("\n"+stars+"\n")
+      else: log.Write("\n"+stars+'*'*14+"\n")
 
       for file in files: # loop over the output files and compute thermochemistry
          bbe = calc_bbe(file, options.QH, options.freq_cutoff, options.temperature, options.conc, options.freq_scale_factor, options.solv, options.spc)
 
+         if options.xyz == True: # write Cartesians
+             xyzdata = getoutData(file)
+             xyz.Writetext(str(len(xyzdata.ATOMTYPES)))
+             if hasattr(bbe, "scf_energy"): xyz.Writetext('{:<39} {:>13} {:13.6f}'.format(file.split(".")[0], 'Eopt', bbe.scf_energy))
+             else: xyz.Writetext('{:<39}'.format(file.split(".")[0]))
+             if hasattr(xyzdata, 'CARTESIANS') and hasattr(xyzdata, 'ATOMTYPES'): xyz.Writecoords(xyzdata.ATOMTYPES, xyzdata.CARTESIANS)
+
          log.Write("\no  "+'{:<39}'.format(file.split(".")[0]))
          if options.spc != False:
-            if hasattr(bbe, "sp_energy"): log.Write(' {:13.6f}'.format(bbe.sp_energy))
-            else: log.Write(' {:>13}'.format('----'))
+            try: log.Write(' {:13.6f}'.format(bbe.sp_energy))
+            except ValueError: log.Write(' {:>13}'.format('----'))
          if hasattr(bbe, "scf_energy"): log.Write(' {:13.6f}'.format(bbe.scf_energy))
          if not hasattr(bbe,"gibbs_free_energy"): log.Write("   Warning! Couldn't find frequency information ...")
          else:
@@ -463,3 +512,4 @@ if __name__ == "__main__":
 
    # close the log
    log.Finalize()
+   if options.xyz == True: xyz.Finalize()
