b8dfb2409547c04247fefaaf62aa99697bd1c321
Robert Paton <bobbypaton@users.noreply.github.com>
2018-04-13 08:11:32 -0600

    Update GoodVibes.py

--- goodvibes/GoodVibes.py
+++ goodvibes/GoodVibes.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python
-from __future__ import print_function
+from __future__ import print_function, absolute_import
 
 #######################################################################
 #                              GoodVibes.py                           #
