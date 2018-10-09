b5ada735fc1fae7cbcc95be440ca0c6b4ebb099a
Robert Paton <robert.paton@colostate.edu>
2018-10-09 00:54:06 -0600

    Revert "Delete setup.py"
    This reverts commit 9aff42f0c7b1a90d775577d67c72719e14011392.

---
+++ setup.py
@@ -0,0 +1,25 @@
+from setuptools import setup
+
+# read the contents of your README file
+from os import path
+this_directory = path.abspath(path.dirname(__file__))
+with open(path.join(this_directory, 'README.md'), encoding='utf-8') as f:
+    long_description = f.read()
+
+setup(
+  name = 'goodvibes',
+  packages = ['goodvibes'],
+  version = '2.0.3',
+  description = 'Calculates quasi-harmonic free energies from Gaussian output files with temperature and haptic corrections',
+  long_description=long_description,
+  long_description_content_type='text/markdown'
+  author = 'Paton Research Group',
+  author_email = 'robert.paton@colostate.edu',
+  url = 'https://github.com/bobbypaton/goodvibes',
+  download_url = 'https://github.com/bobbypaton/GoodVibes/archive/2.0.3.zip',
+  keywords = ['compchem', 'thermochemistry', 'gaussian', 'vibrational-entropies', 'temperature'],
+  classifiers = [],
+  install_requires=["numpy", ],
+  python_requires='>=2.6',
+  include_package_data=True,
+)
