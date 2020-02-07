cefa3c1ca752a86b6c4b371bd07efc0e44767d21
luchini18 <Guilian.Luchini@colostate.edu>
2020-02-07 11:29:40 -0700

    TravisCI updates

--- .travis-install.sh
+++ .travis-install.sh
@@ -7,10 +7,12 @@ if [[$TRAVIS_OS_NAME == 'osx']]; then
         py26)
             pyenv install 2.6.9
             export PYENV_VERSION=2.6.9
+            python -m pip install 'numpy=1.14.2'
             ;;
         py27)
             pyenv install 2.7.12
             export PYENV_VERSION=2.7.12
+            python -m pip install 'numpy=1.14.2'
             ;;
         py35)
             pyenv install 3.5.2
@@ -23,6 +25,10 @@ if [[$TRAVIS_OS_NAME == 'osx']]; then
         py37)
             pyenv install 3.7.2
             export PYENV_VERSION=3.7.2
+            ;;
+        py38)
+            pyenv install 3.8.1
+            export PYENV_VERSION=3.8.1
             ;;
     esac
     export PATH="/Users/travis/.pyenv/shims:${PATH}"
