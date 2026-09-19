54b83fa28a66f47bdc64d69a5ed9a7aa4b96d87a
bobbypaton <321573+bobbypaton@users.noreply.github.com>
2026-09-19 13:55:12 -0600

    Docs: version 4.4.0

--- docs/conf.py
+++ docs/conf.py
@@ -25,9 +25,9 @@ copyright = '2020-2026, Luchini, G.; Alegre-Requena J. V.; Guan, Y.; Funes-Ardoi
 author = 'Luchini, G.; Alegre-Requena J. V.; Guan, Y.; Funes-Ardoiz, I.; Paton, R. S.'
 
 # The short X.Y version
-version = '4.3'
+version = '4.4'
 # The full version, including alpha/beta/rc tags
-release = '4.3.0'
+release = '4.4.0'
 
 
 # -- General configuration ---------------------------------------------------
